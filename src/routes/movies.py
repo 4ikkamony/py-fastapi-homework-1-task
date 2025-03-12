6802ee240e051f2f1210423b9532e33e11817977
4ikkamony <compbanderloga@gmail.com>
2025-03-12 19:27:09 +0200

    Add PaginationParams
    Encapsualte pagination params in a pydantic model to validate them and
    reuse if needed
    
    Fix next_page and prev_page urls

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,60 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, HTTPException, Request
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
+from schemas import (
+    MovieListResponseSchema,
+    MovieDetailResponseSchema,
+    PaginationParams
+)
 
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+    request: Request,
+    pagination: PaginationParams = Depends(),
+    db: AsyncSession = Depends(get_db),
+):
+    total_items = await db.scalar(select(func.count(MovieModel.id)))
+
+    total_pages = (total_items + pagination.per_page - 1) // pagination.per_page
+
+    if total_items == 0 or pagination.page > total_pages:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    query = select(MovieModel).offset(pagination.offset).limit(pagination.per_page)
+    result = await db.execute(query)
+
+    movies = result.scalars().all()
+
+    prev_page = (
+        str(request.url.include_query_params(page=pagination.page - 1))
+        if pagination.page > 1 else None
+    )
+    next_page = (
+        str(request.url.include_query_params(page=pagination.page + 1))
+        if pagination.page < total_pages else None
+    )
+
+    return {
+        "movies": movies,
+        "prev_page": prev_page,
+        "next_page": next_page,
+        "total_pages": total_pages,
+        "total_items": total_items
+    }
+
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    movie = await db.get(MovieModel, movie_id)
+    if not movie:
+        raise HTTPException(
+            status_code=404,
+            detail="Movie with the given ID was not found."
+        )
+    return movie
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,38 @@
-# Write your code here
+import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, Field, AnyHttpUrl, computed_field
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[AnyHttpUrl]
+    next_page: Optional[AnyHttpUrl]
+    total_pages: int
+    total_items: int
+
+
+class PaginationParams(BaseModel):
+    page: int = Field(1, ge=1, description="Page number >= 1")
+    per_page: int = Field(10, ge=1, le=20, description="Number of movies per page")
+
+    @computed_field
+    @property
+    def offset(self) -> int:
+        return (self.page - 1) * self.per_page
